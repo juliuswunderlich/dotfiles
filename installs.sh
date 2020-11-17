e31249b4548862565c3830bf790955e3734e3c1e
Julius W <julius.wunderlich@web.de>
2020-11-18 00:09:49 +0100

    started work on install script

--- installs.sh
+++ installs.sh
@@ -1,4 +1,8 @@
+#!/bin/sh
+
+# -----------------------------------------------------------------------
 # install zsh
+# -----------------------------------------------------------------------
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         # Linux
         sudo apt install git-core zsh
@@ -9,5 +13,23 @@ else
         #unknown
 fi
 
+# -----------------------------------------------------------------------
 # install oh my zsh
+# -----------------------------------------------------------------------
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
+# check current location
+if [ "$PWD" = "$HOME/dotfiles"]; then
+
+
+else
+  echo "Repo must be placed in HOME!"
+fi
+
+
+setup_bash()
+{
+
+}
+
